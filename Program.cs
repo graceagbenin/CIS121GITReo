670420d703e12a06ff9ec99a9676193fe8a2b2a2
Grace Agbenin <ofureagbenin@gmail.com>
2025-01-30 15:14:21 -0600

    Initial Commit
    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace QuizCalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuizCalculatorA aQuizCalculatorA = new QuizCalculatorA();
+            aQuizCalculatorA.Quiz1 = 80.0;
+            aQuizCalculatorA.Quiz2 = 85.0;
+            aQuizCalculatorA.Quiz3 = 90.0;
+            aQuizCalculatorA.Quiz4 = 85.0;
+
+
+
+
+            Console.WriteLine("Total:" + aQuizCalculatorA.CalcTotal{ }) ;
+        }
+    }
+}
